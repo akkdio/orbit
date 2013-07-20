34a342c8d0b178abab78c5af920209be6ec9e0be
akkdio <akkdio@gmail.com>
2013-07-20 14:55:22 -0400

    commiting the whole demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orbit::Application.config.secret_token = 'dba2a2fc887e48bbde3d1ca9459c646333bbd125a71873253a75093185580f7e1d6ae4ffd1fed0ec5dcbf6ec5537775f3fba5b86ba33b7a774a919a8af3c0515'
